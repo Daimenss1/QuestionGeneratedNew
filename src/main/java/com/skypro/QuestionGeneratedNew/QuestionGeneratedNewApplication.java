6598c14d8304feb61096970cf220e13d2ce24573
dgobozov <saniamit@mail.ru>
2022-05-30 15:27:54 +0300

    KR2

---
+++ src/main/java/com/skypro/QuestionGeneratedNew/QuestionGeneratedNewApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.QuestionGeneratedNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionGeneratedNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionGeneratedNewApplication.class, args);
+	}
+
+}
